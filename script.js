0a4ae1316d06e502c43545a7eb3fcc0e37c8ae3a
Ruslan Bogdanov <russian.bogd@gmail.com>
2022-04-16 19:02:39 +0300

    fix: changed burger menu script

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const burgerButton = document.querySelector('.burger'),
     headerNav = document.querySelector('.header-nav'),
-    body = document.querySelector('body');
+    body = document.querySelector('body'),
+    navItems = document.querySelectorAll('.nav-item');
 
-burgerButton.addEventListener('click', () => {
-    burgerButton.classList.toggle('burger-active');
-    headerNav.classList.toggle('header-nav-active');
-    body.classList.toggle('body-burger-active');
-})
+function burgerMenu() {
+    if (!burgerButton.classList.contains('burger-active')) {
+        burgerButton.classList.add('burger-active');
+    } else {
+        burgerButton.classList.remove('burger-active');
+    }
+
+    if (!headerNav.classList.contains('header-nav-active')) {
+        headerNav.classList.add('header-nav-active');
+    } else {
+        headerNav.classList.remove('header-nav-active');
+    }
+
+    if (!body.classList.contains('body-burger-active')) {
+        body.classList.add('body-burger-active');
+    } else {
+        body.classList.remove('body-burger-active');
+    }
+}
+
+burgerButton.addEventListener('click', () => burgerMenu());
+navItems.forEach(e => {e.addEventListener('click', () => burgerMenu())})
